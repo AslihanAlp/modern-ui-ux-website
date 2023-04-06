aad09c065f309f0d316a962c954102de8cebebcc
Aslıhan Alp Şahin <113977691+AslihanAlp@users.noreply.github.com>
2023-04-06 20:56:43 +0300

    padding fixed

--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -6,8 +6,7 @@ const Blog = () => {
   return (
     <div className="gbt3__blog section__padding" id="blog">
       <div className="gbt3__blog-heading">
-        <h1 className="gradient_text">A lot is happening, 
-            We are blogging about it.
+        <h1 className="gradient_text">A lot is happening, We are blogging about it.
         </h1>
       </div>
       <div className="gbt3__blog-container">
